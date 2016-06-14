e7343f207db640da7c050b933934eae8d307ab1e
Aquerr <nerdianin@gmail.com>
2016-06-15 01:15:39 +0200

    Basic code for menu's buttons.

---
+++ XNA-Game-Testing/XNA_Game_Testing/Classes/Button.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace XNA_Game_Testing.Classes
+{
+    class Button
+    {
+        Texture2D texture;
+        Vector2 position;
+        Rectangle rectangle;
+
+        Color colour = new Color(255, 255, 255, 255);
+
+        public Vector2 size;
+
+        public Button(Texture2D newTexture, GraphicsDevice graphics)
+        {
+
+            texture = newTexture;
+
+            size = new Vector2(graphics.Viewport.Width / 8, graphics.Viewport.Height / 30);
+
+        }
+
+        bool down;
+        public bool isClicked;
+        public void Update(MouseState mouse)
+        {
+            rectangle = new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y);
+
+            Rectangle mouseRectangle = new Rectangle(mouse.X, mouse.Y, 1, 1);
+        }
+    }
+}
